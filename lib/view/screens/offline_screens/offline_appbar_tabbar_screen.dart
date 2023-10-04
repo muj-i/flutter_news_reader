666254b37c259d78e49b7d95ce8957aa51d1ac91
MUJ <99077742+muj-i@users.noreply.github.com>
2023-10-04 21:41:03 +0600

    add online offline indicator

--- lib/view/screens/offline_screens/offline_appbar_tabbar_screen.dart
+++ lib/view/screens/offline_screens/offline_appbar_tabbar_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_news_reader/view/screens/offline_screens/offline_allnews_screen.dart';
 import 'package:flutter_news_reader/view/screens/offline_screens/offline_breakingnews_screen.dart';
+import 'package:flutter_news_reader/view/widgets/icon_with_collapsing_text.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class OfflineAppBarTabBArScreen extends StatefulWidget {
   const OfflineAppBarTabBArScreen({Key? key}) : super(key: key);
@@ -17,7 +19,15 @@ class _OfflineAppBarTabBArScreenState extends State<OfflineAppBarTabBArScreen> {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          centerTitle: true,
+          actions: [
+            IconWithCollapsingText(
+              iconData: FontAwesomeIcons.earthAsia,
+              text: 'Offline',
+              alternativeIconData: FontAwesomeIcons.globe,
+              containerColor: Colors.red.withOpacity(.3),
+            )
+          ],
+          centerTitle: false,
           title: const Text('Flutter News Reader'),
           bottom: const TabBar(
             tabs: [
